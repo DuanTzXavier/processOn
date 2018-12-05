93e4260576436e8eb13e99f3bdc7484fd216cf61
bask <baskcatsayhi@gmail.com>
2018-12-05 11:38:21 +0800

    best draw line for ever
    Change-Id: I2b5ac74426ff9a9e8f21ab71253553a2e810bb9f

--- process-start/src/LinkedArrow.js
+++ process-start/src/LinkedArrow.js
@@ -10,12 +10,6 @@ class LinkedArrow extends Component {
         this.state = {
             isActive: false,
             bindLink: this.props.bindLink,
-            style: {
-                left: "146px",
-                top: "156px",
-                width: "10px",
-                height: "16px"
-            },
         }
         console.log(this.props.bindLink)
         console.log("reactCallback1")
@@ -23,12 +17,12 @@ class LinkedArrow extends Component {
         this.initArrow = this.initArrow.bind(this)
     }
 
-    updateProps(bindLink){
+    updateProps(bindLink) {
         bindLink.that.setState({
             bindLink: bindLink,
         });
     }
-    
+
 
     onMouseDown(e) {
         const clickEvent = window.event || e;
@@ -53,8 +47,8 @@ class LinkedArrow extends Component {
         const moveEvent = window.event || event;
         var style = this.state.style;
         let _obj = JSON.stringify(style),
-        objClone = JSON.parse(_obj);
-        
+            objClone = JSON.parse(_obj);
+
         objClone.left = moveEvent.clientX - this.state.fromX + "px"
 
         objClone.top = moveEvent.clientY - this.state.fromY + "px"
@@ -73,7 +67,7 @@ class LinkedArrow extends Component {
         // build div style
         let left = Math.min(this.state.bindLink.startPoint.X, this.state.bindLink.endPoint.X) - this.harfArrowWidth + "px"
         let top = Math.min(this.state.bindLink.startPoint.Y, this.state.bindLink.endPoint.Y) - this.harfArrowWidth + "px"
-        
+
         let width = Math.abs(this.state.bindLink.startPoint.X - this.state.bindLink.endPoint.X) + this.harfArrowWidth * 2
         let height = Math.abs(this.state.bindLink.startPoint.Y - this.state.bindLink.endPoint.Y) + this.harfArrowWidth * 2
 
@@ -104,29 +98,59 @@ class LinkedArrow extends Component {
 
     initArrow() {
         const canvas = document.getElementById("arrow_origin")
-        canvas.width = parseInt(this.state.style.width)
-        canvas.height = parseInt(this.state.style.height)
+
+        //config params
+        const isVertical = false
+
+        //initParams
+        let width = Math.abs(this.state.bindLink.startPoint.X - this.state.bindLink.endPoint.X) + this.harfArrowWidth * 2
+        let height = Math.abs(this.state.bindLink.startPoint.Y - this.state.bindLink.endPoint.Y) + this.harfArrowWidth * 2
+
+        canvas.width = width
+        canvas.height = height
+        let startX = this.state.bindLink.startPoint.X;
+        let startY = this.state.bindLink.startPoint.Y;
+        let endX = this.state.bindLink.endPoint.X;
+        let endY = this.state.bindLink.endPoint.Y;
+        let startPosition = (startX < endX ? 0 : 1) | (startY < endY ? 0 : 2)
+        let endPosition = startPosition ^ 3
+
+        let haw = this.harfArrowWidth
 
         //获取对应的CanvasRenderingContext2D对象(画笔)
         var ctx = canvas.getContext("2d");
 
-        const LINE_WIDTH = 7;
-
-        const isVertical = true;
-
-        //如果 canvas 宽度大于 线宽则为折线，否则为直线
-        if (canvas.width > LINE_WIDTH) {
-            ctx.moveTo(10, 0);
-            ctx.lineTo(10, canvas.height);
-        } else {
-            //直线
-            ctx.moveTo(10, 0);
-            ctx.lineTo(10, canvas.height);
-
-            // point = getPointByIsVertical(10, canvas.height, isVertical)
-            // var angle = isVertical ? DOWN_ARROW : RIGHT_ARROW;
-            // drawLastArrow(ctx, point, angle)
+        let points = []
+        console.log("startPosition" + startPosition)
+        console.log("endPosition" + endPosition)
+        points[0] = {
+            x: (startPosition & 1 == 1) ? (canvas.width - haw) : haw,
+            y: startPosition < 2 ? haw : canvas.height - haw
         }
+
+        points[1] = {
+            x: isVertical ? (startPosition & 1 == 1) ? (canvas.width - haw) : haw : canvas.width / 2,
+            y: isVertical ? canvas.height / 2 : startPosition < 2 ? haw : canvas.height - haw,
+        }
+
+        points[2] = {
+            x: isVertical ? (startPosition & 1 == 1) ? haw : (canvas.width - haw) : canvas.width / 2,
+            y: isVertical ? canvas.height / 2 : startPosition > 1 ? haw : canvas.height - haw,
+        }
+
+        points[3] = {
+            x: (endPosition & 1 == 1) ? (canvas.width - haw) : haw,
+            y: endPosition < 2 ? haw : canvas.height - haw
+        }
+
+        console.log(points)
+
+        ctx.moveTo(points[0].x, points[0].y)
+        points.forEach((value, _) => {
+            ctx.lineTo(value.x, value.y)
+        })
+
+
         //沿着坐标点顺序的路径绘制直线
         ctx.stroke();
         //关闭当前的绘制路径
